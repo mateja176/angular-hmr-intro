5cc2fa11bbc7e1d834a1532e648d712da644c0dc
mytee306 <mateja176@gmail.com>
2019-03-08 12:09:50 +0100

    Updates main.ts to support hmr

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+//   if you don't want to add `hmr: false` to every environment
+if ((environment as any).hmr) {
+  //   throws type error even if @types/webpack-env is installed
+  if ((module as any).hot) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
